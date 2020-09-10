9694623fff21efea85a66501d5332f2b35cfb3a6
gmelektronik <david.stalder@gm-elektronik.swiss>
2020-09-10 16:55:47 +0200

    draws a simple triangle to the screen using ASCII signs

---
+++ src/App/shape.java
@@ -0,0 +1,10 @@
+package App;
+
+public class shape {
+	public static void drawing_shape(String [] args) {
+		System.out.println("   /|");
+		System.out.println("  / |");
+		System.out.println(" /  |");
+		System.out.println("/___|");
+	}
+}
