306783717388b956248f5efe144ef01bacd9e4fd
gmelektronik <david.stalder@gm-elektronik.swiss>
2020-09-23 17:25:55 +0200

    preset application file

--- src/App/Application.java
+++ src/App/Application.java
@@ -1,7 +1,14 @@
 package App;
 
+import java.util.Scanner;
+
 public class Application {
 	public static void main(String [] args) {
 		
+		Scanner keyboardInput = new Scanner(System.in);
+		
+		
+		
+		System.out.println();
 	}
 }
